1a69bc18593b3ba97e66178db95e4f8889ca265d
Abigail M. Rodriguez <abigail.mrodriguez21@gmail.com>
2022-06-08 21:30:52 -0500

    Sección 25 Eclat en el curso de Machine Learning en Udemy

---
+++ Part 5 - Association Rule Learning/Section 29 - Eclat/eclat.R
@@ -0,0 +1,23 @@
+## reglas de asociación Eclat
+#Combinación de apriori, solo se selecciona el soporte mínimo a utilizar 
+#Implica conjuntos de productos comprado en pares
+
+
+#preprocesado de datos
+library(arules) 
+library(arulesViz)
+
+dataset = read.csv("Market_Basket_Optimisation.csv", header = FALSE)
+dataset = read.transactions("Market_Basket_Optimisation.csv",
+                            sep = ",", rm.duplicates = TRUE)
+summary(dataset)
+itemFrequencyPlot(dataset, topN = 10)
+
+
+# Entrenar algoritmo Eclat con el dataset
+rules = eclat(data = dataset, 
+                parameter = list(support = 0.004, minlen=2))
+
+# Visualización de los resultados
+inspect(sort(rules, by = 'support')[1:10])
+
